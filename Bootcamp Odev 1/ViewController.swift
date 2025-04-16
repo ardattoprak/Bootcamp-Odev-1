daaabfa952069da584a84a9d830883a7b28c832d
Arda Toprak <113794954+ardattoprak@users.noreply.github.com>
2025-04-16 18:11:08 +0300

    First commit

--- Bootcamp Odev 1/ViewController.swift
+++ Bootcamp Odev 1/ViewController.swift
@@ -12,8 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        self.navigationItem.title = "Toprak Airlines"
+            
+        let appearence = UINavigationBarAppearance()
+        
+        appearence.backgroundColor = UIColor(named: "mainColor")
+        
+        appearence.titleTextAttributes = [.foregroundColor: UIColor(named: "textColor1")!,.font: UIFont(name: "Tangerine-Regular", size: 30)!]
 
+        
+        navigationController?.navigationBar.standardAppearance = appearence
+        navigationController?.navigationBar.compactAppearance = appearence
+        navigationController?.navigationBar.scrollEdgeAppearance = appearence
+        
+    }
 
 }
 
