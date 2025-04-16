6ccd050205ae70a63b758bd07b21124c21504521
Arda Toprak <113794954+ardattoprak@users.noreply.github.com>
2025-04-16 16:14:34 +0300

    Initial Commit

---
+++ Bootcamp Odev 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bootcamp Odev 1
+//
+//  Created by Arda Toprak on 16.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
